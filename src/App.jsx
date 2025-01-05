562b4c4685c2c060cf990ddcf18103131369dbb0
riuteshyaaa <riteshyaaa@gmail.com>
2025-01-05 22:03:59 +0530

    taiwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+const  App = () => {
+ 
+
+  return (
+ 
+      <div>
+        <h1 className='font-bold text-red-700' >   
+            This is Netflix project </h1>
+      </div>
+       
+   
+  )
+}
+
+export default App
