5b110139ea05bdc690b158ff86ca57cc163c6314
riuteshyaaa <riteshyaaa@gmail.com>
2025-01-11 01:02:44 +0530

    Add useNowPlayingMovies hook and integrate with Redux store for fetching now playing movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constent";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+
+
+
+
 const Browse = () => {
-
-
-  const getNowplayingmovies = async() =>{
-    // API call to fetch now playing movies
-   const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
-    const json =  await data.json()
-   console.log(json)
-
-  }
-  useEffect(() => {
-    getNowplayingmovies();
-  }, []);
- 
+  useNowPlayingMovies()
   return <div>
     <Header/>
 
---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { API_OPTIONS } from "../utils/constent";
+
+const useNowPlayingMovies = () => {
+    const dispatch = useDispatch()
+  const getNowplayingmovies = async() =>{
+    // API call to fetch now playing movies
+   const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
+    const json =  await data.json()
+   console.log(json.results)
+    dispatch(addNowPlayingMovies(json.results))
+  }
+  useEffect(() => {
+    getNowplayingmovies();
+  }, []);
+ 
+}
+export default useNowPlayingMovies;
