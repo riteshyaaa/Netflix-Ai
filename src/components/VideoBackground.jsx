dfc37810279ebb3efda1a9924dda2ab613b36fcf
riuteshyaaa <riteshyaaa@gmail.com>
2025-01-12 01:40:35 +0530

    Add SecondaryContainer component; update VideoBackground for improved video display

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import useMoviesTrailer from "../hooks/useMoviesTrailer";
 
-
-
 const VideoBackground = ({ movieId }) => {
-
-    const  trailerVideo = useSelector((store) => store.movies.trailerVideo)
- useMoviesTrailer(movieId)
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+  useMoviesTrailer(movieId);
   return (
-    <div>
+    <div className="w-screen aspect-video ">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key }
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key 
+          +"&autoplay=1 &mute=1"
+        }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
