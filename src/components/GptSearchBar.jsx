f87090d92665c1bf15330149e7839d4d8662a156
riuteshyaaa <riteshyaaa@gmail.com>
2025-01-18 00:29:59 +0530

    Refactor GptSearchBar component to implement search functionality using useRef and prevent default form submission

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
 
 const GptSearchBar = () => {
   const langKey =  useSelector((store) => store.config.language)
-  console.log(langKey)
+  const searchText = useRef(null);
+  // console.log(langKey)
+  const handleGptSearch =  () => {
+   console.log(searchText.current.value)
+  }
   return (
     <div className="pt-[8%] m-2 flex justify-center">
-      <form className=" px-2  w-1/2 grid grid-cols-12 bg-black ">
+      <form className=" px-2  w-1/2 grid grid-cols-12 bg-black " onSubmit = {(e) => e.preventDefault()}>
         <input
+        ref =  {searchText}
           className=" p-2 m-2 col-span-9"
           type="text"
           placeholder={lang[langKey].gptPlaceholder}
         />
         <button
           className="py-2 px-4  m-3 col-span-3 bg-red-500 rounded  text-white"
-          type="submit"
+          
+          onClick={handleGptSearch}
         >
         {lang[langKey].search}
         </button>
